99ea62d1f65948703a48f4b8fa7baa4e0c15dc3e
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-05-29 23:49:58 -0400

    Created function to calculate the conversion from and to units.

--- UnitConversionApp/MainPage.xaml.cs
+++ UnitConversionApp/MainPage.xaml.cs
@@ -10,49 +10,49 @@ public partial class MainPage : ContentPage
 		fromUnit.SelectedIndex = 0;
 	}
 
-	private void startIndexChanged(object sender, EventArgs e)
-	{
-		string from_unit = fromUnit.SelectedItem?.ToString();
-		if(from_unit != null)
-		{
-			toUnit.Items.Clear();
-			switch (from_unit)
-			{
-				case "MPH":
-					toUnit.Items.Add("KPH");
-					break;
+    private void startIndexChanged(object sender, EventArgs e)
+    {
+        string from_unit = fromUnit.SelectedItem?.ToString();
+        if (from_unit != null)
+        {
+            toUnit.Items.Clear();
+            switch (from_unit)
+            {
+                case "MPH":
+                    toUnit.Items.Add("KPH");
+                    break;
                 case "KPH":
                     toUnit.Items.Add("MPH");
                     break;
-				default:
-					break;
-				
+                default:
+                    break;
+
             }
             toUnit.SelectedIndex = 0;
         }
-	}
+    }
 
-	private void onButtonClicked(object sender, EventArgs e)
-	{
-		double result = 0;
-		string from_unit_selected = fromUnit.SelectedItem?.ToString();
-		string to_unit_selected = toUnit.SelectedItem?.ToString();
-		if(from_unit_selected == null || to_unit_selected == null)
-		{
-			OutputLabel.Text = "Please select unit";
-			return;
-		}
-		if(!double.TryParse(userInput.Text, out double value))
-		{
-			OutputLabel.Text = "Please enter valid number";
-			return;
-		}
+    private void onButtonClicked(object sender, EventArgs e)
+    {
+        double result = 0;
+        string from_unit_selected = fromUnit.SelectedItem?.ToString();
+        string to_unit_selected = toUnit.SelectedItem?.ToString();
+        if (from_unit_selected == null || to_unit_selected == null)
+        {
+            OutputLabel.Text = "Please select unit";
+            return;
+        }
+        if (!double.TryParse(userInput.Text, out double value))
+        {
+            OutputLabel.Text = "Please enter valid number";
+            return;
+        }
 
-		if(from_unit_selected == "MPH" & to_unit_selected == "KPH")
-		{
-			result = ConvertToMPHFromKPH(value);
-			OutputLabel.Text = result.ToString();
-		}
+        if (from_unit_selected == "MPH" & to_unit_selected == "KPH")
+        {
+            result = ConvertToMPHFromKPH(value);
+            OutputLabel.Text = result.ToString();
+        }
         if (from_unit_selected == "KPH" & to_unit_selected == "MPH")
         {
             result = ConvertToKPHFromMPH(value);
@@ -60,13 +60,13 @@ public partial class MainPage : ContentPage
         }
     }
 
-    //converts to KPHF
+    //converts to KPHfromMPH
     private double ConvertToKPHFromMPH(double value)
     {
         return value / 1.60934;
     }
 
-    //converts to MPHF
+    //converts to MPHfromKPH
     private double ConvertToMPHFromKPH(double value)
     {
         return value * 1.60934;
