cbf9f2d38c5906c96c883ecea83b05f3dd76a9c9
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-05-29 23:49:12 -0400

    Created UI for the app.

--- UnitConversionApp/MainPage.xaml.cs
+++ UnitConversionApp/MainPage.xaml.cs
@@ -2,24 +2,75 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	
 
 	public MainPage()
 	{
 		InitializeComponent();
+		fromUnit.SelectedIndex = 0;
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private void startIndexChanged(object sender, EventArgs e)
 	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		string from_unit = fromUnit.SelectedItem?.ToString();
+		if(from_unit != null)
+		{
+			toUnit.Items.Clear();
+			switch (from_unit)
+			{
+				case "MPH":
+					toUnit.Items.Add("KPH");
+					break;
+                case "KPH":
+                    toUnit.Items.Add("MPH");
+                    break;
+				default:
+					break;
+				
+            }
+            toUnit.SelectedIndex = 0;
+        }
 	}
+
+	private void onButtonClicked(object sender, EventArgs e)
+	{
+		double result = 0;
+		string from_unit_selected = fromUnit.SelectedItem?.ToString();
+		string to_unit_selected = toUnit.SelectedItem?.ToString();
+		if(from_unit_selected == null || to_unit_selected == null)
+		{
+			OutputLabel.Text = "Please select unit";
+			return;
+		}
+		if(!double.TryParse(userInput.Text, out double value))
+		{
+			OutputLabel.Text = "Please enter valid number";
+			return;
+		}
+
+		if(from_unit_selected == "MPH" & to_unit_selected == "KPH")
+		{
+			result = ConvertToMPHFromKPH(value);
+			OutputLabel.Text = result.ToString();
+		}
+        if (from_unit_selected == "KPH" & to_unit_selected == "MPH")
+        {
+            result = ConvertToKPHFromMPH(value);
+            OutputLabel.Text = result.ToString();
+        }
+    }
+
+    //converts to KPHF
+    private double ConvertToKPHFromMPH(double value)
+    {
+        return value / 1.60934;
+    }
+
+    //converts to MPHF
+    private double ConvertToMPHFromKPH(double value)
+    {
+        return value * 1.60934;
+    }
 }
 
 
